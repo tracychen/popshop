b2a64f5b83f0eb134254a21ed90689fbd964ef21
tracy <tracychen0711@gmail.com>
2024-06-30 13:55:19 -0700

    fix(apps/web): update chain for preview env

--- apps/web/lib/chain.ts
+++ apps/web/lib/chain.ts
@@ -4,7 +4,7 @@ function getChain() {
   const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
   if (env === "production") {
     return base;
-  } else if (env === "development") {
+  } else if (env === "preview") {
     return baseSepolia;
   } else {
     return anvil;
