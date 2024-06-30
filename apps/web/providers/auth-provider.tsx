fcead454c4209749818684ebb9cb2f687722cb09
tracy <tracychen0711@gmail.com>
2024-06-30 16:17:35 -0700

    fix(apps/web): set default chain and supported chains in privy provider

--- apps/web/providers/auth-provider.tsx
+++ apps/web/providers/auth-provider.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { chain } from "@/lib/chain";
 import { PrivyProvider } from "@privy-io/react-auth";
+import { anvil, base, baseSepolia } from "viem/chains";
 
 export function AuthProvider({ children }: { children?: React.ReactNode }) {
   return (
     <PrivyProvider
       appId="clxo3etkc00obqo7nwp6o33ra"
       config={{
+        defaultChain: chain,
+        supportedChains: [base, baseSepolia, anvil],
         externalWallets: {
           coinbaseWallet: {
             connectionOptions: "smartWalletOnly",
